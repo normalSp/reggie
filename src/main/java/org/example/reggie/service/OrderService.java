168b29bb4e999cd0734112aa5b0b7f771796da7a
PlumSnow <1141138286@qq.com>
2024-03-25 15:29:55 +0800

    pom删了yonyou的一个依赖

--- src/main/java/org/example/reggie/service/OrderService.java
+++ src/main/java/org/example/reggie/service/OrderService.java
@@ -1,7 +1,6 @@
 package org.example.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import io.vavr.collection.Tree;
 import org.example.reggie.entity.Orders;
 
 public interface OrderService extends IService<Orders> {
