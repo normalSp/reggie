1f66fe15f991939aaf604a308ffc5e5ebf21c34a
PlumSnow <1141138286@qq.com>
2024-03-26 17:21:14 +0800

    配置Spring Cache环境

--- src/main/java/org/example/reggie/controller/DishController.java
+++ src/main/java/org/example/reggie/controller/DishController.java
@@ -15,6 +15,7 @@ import org.example.reggie.service.DishFlavorService;
 import org.example.reggie.service.DishService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
